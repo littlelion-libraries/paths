80ede90e01a5b60815db5d6a000f98c43976c62b
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-07 22:06:42 +0700

    1.1.0

--- PathUtils.cs
+++ PathUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 
 namespace Paths
@@ -7,23 +8,28 @@ namespace Paths
     {
         public static void Simplify(Queue<Vector3> des, int length, Vector3[] src)
         {
+            Simplify(des, length, i => src[i]);
+        }
+
+        public static void Simplify(Queue<Vector3> des, int length, Func<int, Vector3> src)
+        {
             if (length < 3)
             {
                 for (var i = 0; i < length; i++)
                 {
-                    des.Enqueue(src[i]);
+                    des.Enqueue(src(i));
                 }
 
                 return;
             }
 
-            var next = src[1];
-            var peek = src[0];
+            var next = src(1);
+            var peek = src(0);
             var direction = next - peek;
             des.Enqueue(peek);
             for (var i = 2; i < length; i++)
             {
-                var it = src[i];
+                var it = src(i);
                 var itDelta = it - peek;
                 if (Vector3.Dot(itDelta, direction) != 0 &&
                     Vector3.Multiply(itDelta, direction).LengthSquared() != 0) continue;
